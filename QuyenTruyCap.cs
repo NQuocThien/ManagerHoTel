23df6f27d4bcd9e5dc47828693392f4c6a9bfd44
thien <quocthienorg@gmail.com>
2022-11-03 08:47:09 +0700

    from QuyenTruyCap

---
+++ QuyenTruyCap.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manager_Hotel
+{
+    public partial class QuyenTruyCap : Form
+    {
+        public QuyenTruyCap()
+        {
+            InitializeComponent();
+        }
+
+        private void QuyenTruyCap_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
